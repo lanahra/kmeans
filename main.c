782e4df19cd804f5b359ee27ef26565f4b4c60cd
Leonardo Nahra <leonardonahra@gmail.com>
2018-07-01 11:34:19 -0300

    Add duplication

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include <unistd.h>
 #include "kmeans.h"
@@ -9,6 +10,8 @@ int main(int argc, char **argv) {
     clock_t begin = clock();
     int opt;
     bool parallel = false;
+
+    remove("/tmp/kmeans/sdc");
 
     while ((opt = getopt(argc, argv, "p")) != -1) {
         switch (opt) {
@@ -33,6 +36,7 @@ int main(int argc, char **argv) {
         char *output_path = argv[optind++];
 
         Kmeans_context *kc = alloc_kmeans_context(k, n, f);
+        Kmeans_context *duplicate = alloc_kmeans_context(k, n, f);
 
         FILE *input = fopen(input_path, "r");
         if (input == 0) {
@@ -44,6 +48,7 @@ int main(int argc, char **argv) {
         for (i = 0; i < kc->n; i++) {
             for (j = 0; j < kc->f; j++) {
                 fscanf(input, "%lf ", &kc->observations[i][j]);
+                duplicate->observations[i][j] = kc->observations[i][j];
             }
             fscanf(input, "\n");
         }
@@ -53,6 +58,7 @@ int main(int argc, char **argv) {
         for (i = 0; i < kc->k; i++) {
             for (j = 0; j < kc->f; j++) {
                 fscanf(input, "%lf ", &kc->centroids[i][j]);
+                duplicate->centroids[i][j] = kc->centroids[i][j];
             }
             fscanf(input, "\n");
         }
@@ -68,18 +74,17 @@ int main(int argc, char **argv) {
 
         printf("read time: %lf\n", time);
 
-
-        begin = clock();
-
-        if (parallel) {
-            kmeans_p(kc);
-        } else {
-            kmeans(kc);
-        }
+        kmeans(kc);
+        kmeans(duplicate);
 
         end = clock();
         time = (double)(end - begin) / CLOCKS_PER_SEC;
         printf("time: %lf\n", time);
+
+        if (memcmp(kc->cluster_map, duplicate->cluster_map, n * sizeof *kc->cluster_map) != 0) {
+            FILE *sdc = fopen("/tmp/kmeans/sdc", "w");
+            fclose(sdc);
+        }
 
         FILE *output = fopen(output_path, "w");
         if (output == 0) {
@@ -91,6 +96,9 @@ int main(int argc, char **argv) {
         }
 
         fclose(output);
+
+        free_kmeans_context(kc);
+        free_kmeans_context(duplicate);
     } else {
         exit(EXIT_FAILURE);
     }
